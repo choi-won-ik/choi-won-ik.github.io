e23c1bb68055f79863586a1c99c988a91fac536e
choi-won-ik <140231082+choi-won-ik@users.noreply.github.com>
2024-02-27 16:30:50 +0900

    email 전송까지 완료

--- assets/js/function.js
+++ assets/js/function.js
@@ -14,7 +14,44 @@ window.onload=()=>{
     $portfolioShow.classList.remove('on')
   });
 
+  const $sendMail = document.getElementById('sendMail');
+
+  function createData() {
+    let sendData = $('#mailForm').serialize();
+    let params = new URLSearchParams(sendData);
+    
+    return params;
+  }
+
+  $sendMail.addEventListener('click',(event)=>{
+
+    event.preventDefault();
+
+    let data = createData(event);
+
+    let name = data.get('name');
+    let email=data.get('email');
+    let subject=data.get('subject');
+    let msg=data.get('msg');
 
 
-
+    $.ajax({
+			type : "post",
+			url : "https://script.google.com/macros/s/AKfycbw_zDFDqACtsKSD__dOQVIUJMxoJzAKJmTkMbEsG4q2z5_TI_qiFTJoIjrsR8IDaUCT/exec",
+			data : JSON.stringify({
+        name : name,
+        email:email,
+        subject:subject,
+        msg:msg
+      }),
+			dataType:"json",
+      // contentType: 'application/json',
+			success : function(data, status, xhr){
+        alert('연락주셔서 감사합니다. 빠른 시간내에 꼭 회신드리도록 하겠습니다! :)');
+			},
+			error : function(jqXHR, textStatus, errorThrown){
+				console.log("error")
+			}
+		});
+  });
 }
