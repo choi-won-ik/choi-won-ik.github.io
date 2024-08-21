b8b9ae199df5b311e81d612c8eae2e00c8f7c690
choi-won-ik <140231082+choi-won-ik@users.noreply.github.com>
2024-08-22 00:16:14 +0900

    Update sub.js

--- assets/js/sub.js
+++ assets/js/sub.js
@@ -1,19 +1,4 @@
 window.onload=()=>{
-  	const $portfolioImg = document.getElementById('portfolioImg');
-  	const $portfolioShow = document.getElementById('portfolioShow');
-
-	goGit=()=>{
-		window.location.href="https://github.com/choi-won-ik/chat";
-	};
-
-	$portfolioImg.addEventListener('mouseover',()=>{
-		$portfolioShow.classList.add('on')
-	});
-
-	$portfolioImg.addEventListener('mouseout',()=>{
-		$portfolioShow.classList.remove('on')
-	});
-
 	const $sendMail = document.getElementById('sendMail');
 
 	function createData() {
